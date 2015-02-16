b2ec27e7d290cb746ab423452aaa326b30b7feb2
H155 <H155@users.noreply.github.com>
2015-02-16 21:16:31 +0200

    Update cachematrix.R
    I added some explanations

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,9 @@
+## # makeCacheMatrix creates a list containing a function to
+# 1. set the value of the matrix
+# 2. get the value of the matrix
+# 3. set the value of inverse of the matrix
+# 4. get the value of inverse of the matrix
+
 makeCacheMatrix <- function(x = numeric()) { # a matrix is as argument
         m <- NULL
         set <- function(y) {
@@ -11,6 +17,11 @@ makeCacheMatrix <- function(x = numeric()) { # a matrix is as argument
              setsolve = setsolve,
              getsolve = getsolve)
 }
+# The cacheSolve function returns the inverse of the matrix. It first checks if
+# the inverse has already been computed. If so, it gets the result and skips the
+# computation. If not, it computes the inverse, sets the value in the cache via
+# setinverse function.
+# This function assumes that the matrix is always invertible.
 
 cacheSolve <- function(x, ...) {
         m <- x$getsolve()
